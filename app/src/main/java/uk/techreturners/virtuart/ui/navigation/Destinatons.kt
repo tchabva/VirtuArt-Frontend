36d15757f7fda4349e9811f5b2496a3af875829b
Tawanda Chabva <tchabva@gmail.com>
2025-06-04 23:32:31 +0100

    feat: create the navigaton destinations

---
+++ app/src/main/java/uk/techreturners/virtuart/ui/navigation/Destinatons.kt
@@ -0,0 +1,53 @@
+package uk.techreturners.virtuart.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+// Tabs are the sub navigation graphs
+@Serializable
+sealed interface Tabs {
+    @Serializable
+    data object Artworks : Tabs
+
+    @Serializable
+    data object Search : Tabs
+
+    @Serializable
+    data object Exhibitions : Tabs
+
+    @Serializable
+    data object Profile : Tabs
+}
+
+// For each Composable Screen
+@Serializable
+sealed interface Screens {
+    @Serializable
+    data object Artworks : Screens
+
+    @Serializable
+    data object Search : Screens
+
+    @Serializable
+    data object Exhibitions : Screens
+
+    @Serializable
+    data object Profile : Screens
+
+    @Serializable
+    data class ViewArtwork(val artworkId: String) : Screens
+
+    @Serializable
+    data class ViewExhibition(val exhibitionId: String) : Screens
+
+    companion object {
+        val screensWithoutBottomNav = listOf(
+            ViewArtwork::class,
+            ViewExhibition::class
+        )
+
+        val screensWithTopAppBar = listOf(
+            ViewArtwork::class,
+            ViewExhibition::class
+        )
+    }
+}
