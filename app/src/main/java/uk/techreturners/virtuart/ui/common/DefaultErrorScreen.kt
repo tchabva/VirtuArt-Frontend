77c720b1555540301312341fdf79e79565d50184
Tawanda Chabva <tchabva@gmail.com>
2025-06-06 23:24:16 +0100

    feat: create the DefaultErrorScreen

---
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultErrorScreen.kt
@@ -0,0 +1,41 @@
+package uk.techreturners.virtuart.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import uk.techreturners.virtuart.R
+
+@Composable
+fun DefaultErrorScreen(responseCode: Int, errorMessage: String?) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            text = stringResource(
+                R.string.error_occurred,
+                responseCode,
+                errorMessage ?: stringResource(R.string.unknown_error)
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultErrorScreenPreview() {
+    DefaultErrorScreen(
+        responseCode = 404,
+        errorMessage = ""
+    )
+}
