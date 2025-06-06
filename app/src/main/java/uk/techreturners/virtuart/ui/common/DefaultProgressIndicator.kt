f9bd657fd40adc4506ae1402187cac806d725e19
Tawanda Chabva <tchabva@gmail.com>
2025-06-06 23:10:37 +0100

    feat: add preview to the DefaultProgressIndicator

--- app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultProgressIndicator.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultProgressIndicator.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -24,4 +25,10 @@ fun DefaultProgressIndicator(){
             trackColor = MaterialTheme.colorScheme.surfaceVariant,
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultProgressIndicatorPreview(){
+    DefaultProgressIndicator()
 }
