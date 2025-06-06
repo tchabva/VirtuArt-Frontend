f62e28ad4ba7715f09a4efe5afd3cd9040b331a0
Tawanda Chabva <tchabva@gmail.com>
2025-06-06 23:07:53 +0100

    feat: create the DefaultProgressIndicator composable

---
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultProgressIndicator.kt
@@ -0,0 +1,27 @@
+package uk.techreturners.virtuart.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DefaultProgressIndicator(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+                .align(Alignment.Center),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
+}
