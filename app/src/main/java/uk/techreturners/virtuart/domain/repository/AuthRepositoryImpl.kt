d7c86f97b61718b7ccd181d5d900c89cde8ede8a
Tawanda Chabva <tchabva@gmail.com>
2025-06-06 22:13:33 +0100

    feat: create the AuthRepositoryImpl#

---
+++ app/src/main/java/uk/techreturners/virtuart/domain/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,64 @@
+package uk.techreturners.virtuart.domain.repository
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import uk.techreturners.virtuart.R
+import uk.techreturners.virtuart.domain.model.SignInResult
+import uk.techreturners.virtuart.domain.model.UserData
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val context: Context,
+    private val credentialManager: CredentialManager
+) : AuthRepository {
+
+    private var currentUser: UserData? = null
+
+    override suspend fun signIn(): SignInResult {
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(context.getString(R.string.web_client_id))
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        return try {
+            val result = credentialManager.getCredential(
+                request = request,
+                context = context
+            )
+
+            val credential = result.credential
+            if (credential is GoogleIdTokenCredential){
+                val userData = UserData(
+                    userId = credential.idToken,
+                    email = credential.id,
+                    displayName = credential.displayName,
+                    profilePicture = credential.profilePictureUri?.toString()
+                )
+                currentUser = userData
+                SignInResult.Success(userData)
+            } else{
+                SignInResult.Error(
+                    "Sign In Error"
+                )
+            }
+        } catch (e: GetCredentialException){
+            SignInResult.Error(e.message?: "Unknown Error")
+        }
+    }
+
+    override fun signOut() {
+        currentUser = null
+    }
+
+    override fun getSignedInUser(): UserData? {
+        return currentUser;
+    }
+}
