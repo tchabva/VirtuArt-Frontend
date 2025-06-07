8dbeea7b42c51c3559b09e6b72a745e353d59290
Tawanda Chabva <tchabva@gmail.com>
2025-06-07 02:19:19 +0100

    build: add GlideCompose compilerArg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ android {
         freeCompilerArgs += listOf(
             "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
             "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
-            "-opt-in=androidx.compose.animation.ExperimentalAnimationApi"
+            "-opt-in=androidx.compose.animation.ExperimentalAnimationApi",
+            "-opt-in=com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi",
         )
     }
     buildFeatures {
