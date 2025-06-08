48a1cc3e00ba9152e6b2a76867adc5b0d1b51d6f
Tawanda Chabva <tchabva@gmail.com>
2025-06-08 22:39:46 +0100

    feat: make the CreateExhibitionRequest string variable nullable

--- app/src/main/java/uk/techreturners/virtuart/data/model/Exhibition.kt
+++ app/src/main/java/uk/techreturners/virtuart/data/model/Exhibition.kt
@@ -30,7 +30,7 @@ data class ExhibitionItem(
 
 data class CreateExhibitionRequest(
     val title: String,
-    val description: String
+    val description: String?
 )
 
 data class UpdateExhibitionRequest(
