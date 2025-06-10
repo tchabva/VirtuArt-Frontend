cc94b5de20be304257f8849973febaf0a7276210
Tawanda Chabva <tchabva@gmail.com>
2025-06-10 19:36:33 +0100

    feat: create the Artwork Models

---
+++ app/src/main/java/uk/techreturners/virtuart/data/model/Artwork.kt
@@ -0,0 +1,33 @@
+package uk.techreturners.virtuart.data.model
+
+data class Artwork(
+    val id: String,
+    val title: String,
+    val artist: String?,
+    val date: String,
+    val displayMedium: String?,
+    val imageUrl: String?,
+    val altImageUrls: List<String>,
+    val description: String,
+    val origin: String?,
+    val category: String,
+    val sourceMuseum: String,
+)
+
+data class ArtworkResult(
+    val id: String,
+    val title: String,
+    val artistTitle: String?,
+    val date: String?,
+    val imageURL: String
+)
+
+data class PaginatedArtworkResults(
+    val totalItems: Int,
+    val pageSize: Int,
+    val totalPages: Int,
+    val currentPage: Int,
+    val hasNext: Boolean,
+    val hasPrevious: Boolean,
+    val data: List<ArtworkResult>,
+)
