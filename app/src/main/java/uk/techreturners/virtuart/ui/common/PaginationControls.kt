5ed6faafc83a0a961fdeaabf57f6be80ce06f1f3
Tawanda Chabva <tchabva@gmail.com>
2025-06-11 03:00:13 +0100

    fix: remove unneeded params

--- app/src/main/java/uk/techreturners/virtuart/ui/common/PaginationControls.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/PaginationControls.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.NavigateBefore
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -19,16 +17,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import uk.techreturners.virtuart.R
 
 @Composable
 fun PaginationControls(
-    totalItems: Int,
-    pageSize: Int,
     totalPages: Int,
     currentPage: Int,
     hasNext: Boolean,
@@ -93,8 +87,6 @@ fun PaginationControls(
 @Composable
 fun PaginationControlsPreview() {
     PaginationControls(
-        totalItems = 10,
-        pageSize = 5,
         totalPages = 2,
         currentPage = 1,
         hasNext = true,
