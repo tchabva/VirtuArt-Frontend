1c61bdc53559bec3c0e223dc668ed151a514ed04
Tawanda Chabva <tchabva@gmail.com>
2025-06-11 23:49:51 +0100

    feat: add private modifier to dialog preview and optimise imports

--- app/src/main/java/uk/techreturners/virtuart/ui/screens/artworkdetail/AddToExhibitionDialog.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/screens/artworkdetail/AddToExhibitionDialog.kt
@@ -1,31 +1,19 @@
 package uk.techreturners.virtuart.ui.screens.artworkdetail
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import uk.techreturners.virtuart.R
 import uk.techreturners.virtuart.data.model.Exhibition
-import uk.techreturners.virtuart.ui.common.ExhibitionItem
 
 @Composable
 fun AddToExhibitionDialog(
@@ -47,7 +35,7 @@ fun AddToExhibitionDialog(
                 style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier.padding(bottom = 8.dp)
             )
-            
+
             exhibitions.forEach { exhibition ->
                 Card(
                     onClick = { onAddToExhibition(exhibition.id) },
@@ -77,7 +65,7 @@ fun AddToExhibitionDialog(
 
 @Preview(showBackground = true)
 @Composable
-fun AddToExhibitionDialogPreview() {
+private fun AddToExhibitionDialogPreview() {
     AddToExhibitionDialog(
         exhibitions = listOf(
             Exhibition(
