06f10a1c6cdb54a8dbeca60de24282778267e7a7
Tawanda Chabva <tchabva@gmail.com>
2025-06-12 14:00:36 +0100

    feat: create AicAdvancedSearchQuery model data class

--- app/src/main/java/uk/techreturners/virtuart/domain/model/Search.kt
+++ app/src/main/java/uk/techreturners/virtuart/domain/model/Search.kt
@@ -1,2 +1,11 @@
 package uk.techreturners.virtuart.domain.model
 
+data class AicAdvancedSearchQuery(
+    val title: String?,
+    val artist: String?,
+    val medium: String?,
+    val category: String?,
+    val relevance: String?,
+    val sort: String?,
+    val limit: String?
+)
