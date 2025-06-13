4b8ce8f54589b56e0a431ffe604f8863ebc8749a
Tawanda Chabva <tchabva@gmail.com>
2025-06-13 18:04:26 +0100

    feat: delete Domain Search file and update the Data SearchQuery models for new backend search implementation

--- app/src/main/java/uk/techreturners/virtuart/data/model/SearchQuery.kt
+++ app/src/main/java/uk/techreturners/virtuart/data/model/SearchQuery.kt
@@ -1,13 +1,20 @@
 package uk.techreturners.virtuart.data.model
 
-data class AicApiElasticSearchQuery(
-    val query: Map<String, Any>,
-    val sort: List<Map<String, Any>>,
-    val size: Int,
-    val page: Int
+data class AdvancedSearchRequest(
+    val title: String? = null,
+    val artist: String? = null,
+    val medium: String? = null,
+    val department: String? = null,
+    val sortBy: String = "Title", // default sort By
+    val sortOrder: String = "Ascending", // default sort Order
+    val source: String? = null,
+    val pageSize: Int = 20,
+    val currentPage: Int = 1
 )
 
 data class BasicSearchQuery(
-    val q: String,
-    val query: Map<String,Any>
+    val query: String? = null,
+    val source: String? = null,
+    val pageSize: Int = 20,
+    val currentPage: Int = 1
 )
