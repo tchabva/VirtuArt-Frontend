71cd67559b51e1fca47d280764621f62ddf97fcc
Tawanda Chabva <tchabva@gmail.com>
2025-06-15 12:35:43 +0100

    feat: add selectable to the rows fpr each item

--- app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultSourceDialog.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultSourceDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,10 +44,14 @@ fun DefaultSourceDialog(
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(vertical = 8.dp),
+                            .padding(vertical = 8.dp)
+                            .selectable(
+                                selected = source == key,
+                            ) { onSourceChanged(key) },
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         RadioButton(
+                            modifier = Modifier.clearAndSetSemantics {  },
                             selected = source == key,
                             onClick = { onSourceChanged(key) },
                             colors = RadioButtonDefaults.colors(
