c36a81da39225c39c5d1fb659a23680b9aa0e452
Tawanda Chabva <tchabva@gmail.com>
2025-06-15 06:16:12 +0100

    code: remove unused imports and whitespace

--- app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultSourceButton.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/common/DefaultSourceButton.kt
@@ -4,26 +4,20 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Museum
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import okio.Source
 import uk.techreturners.virtuart.R
 
 @Composable
@@ -47,7 +41,6 @@ fun DefaultSourceButton(
                 modifier = Modifier.size(20.dp),
                 tint = MaterialTheme.colorScheme.onBackground
             )
-
         }
     }
 }
