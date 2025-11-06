4ef242256cd8fa02d497ca6b4325476a6e8b7f39
Tawanda Chabva <tchabva@gmail.com>
2025-11-06 00:22:11 +0000

    feat: implement Coil image loading for ProfileScreenSignedIn page

--- app/src/main/java/uk/techreturners/virtuart/ui/screens/profile/ProfileScreenContent.kt
+++ app/src/main/java/uk/techreturners/virtuart/ui/screens/profile/ProfileScreenContent.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.SubcomposeAsyncImage
 import com.bumptech.glide.integration.compose.GlideSubcomposition
 import com.bumptech.glide.integration.compose.RequestState
 import uk.techreturners.virtuart.R
@@ -90,36 +91,57 @@ private fun ProfileScreenSignedIn(
         Spacer(modifier = Modifier.height(32.dp))
 
         // Profile Picture
-        GlideSubcomposition(
+//        GlideSubcomposition(
+//            model = signedInState.currentUser?.profilePicture,
+//            modifier = Modifier
+//                .size(120.dp)
+//                .clip(CircleShape),
+//        ) {
+//            when (state) {
+//                RequestState.Failure -> {
+//                    Image(
+//                        painter = painterResource(R.drawable.ic_placeholder_artwork),
+//                        contentDescription = stringResource(R.string.user_profile_picture_description),
+//                        contentScale = ContentScale.Crop
+//                    )
+//                }
+//
+//                RequestState.Loading -> {
+//                    DefaultProgressIndicator()
+//                }
+//
+//                is RequestState.Success -> {
+//                    Image(
+//                        modifier = Modifier
+//                            .fillMaxWidth(),
+//                        painter = painter,
+//                        contentDescription = stringResource(R.string.user_profile_picture_description),
+//                        contentScale = ContentScale.Crop
+//                    )
+//                }
+//            }
+//        }
+
+        SubcomposeAsyncImage(
             model = signedInState.currentUser?.profilePicture,
+            contentDescription = stringResource(R.string.user_profile_picture_description),
             modifier = Modifier
                 .size(120.dp)
                 .clip(CircleShape),
-        ) {
-            when (state) {
-                RequestState.Failure -> {
-                    Image(
-                        painter = painterResource(R.drawable.ic_placeholder_artwork),
-                        contentDescription = stringResource(R.string.user_profile_picture_description),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-
-                RequestState.Loading -> {
-                    DefaultProgressIndicator()
-                }
-
-                is RequestState.Success -> {
-                    Image(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        painter = painter,
-                        contentDescription = stringResource(R.string.user_profile_picture_description),
-                        contentScale = ContentScale.Crop
-                    )
-                }
+            contentScale = ContentScale.Crop,
+            loading = {
+                DefaultProgressIndicator()
+            },
+            error = {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    painter = painter,
+                    contentDescription = stringResource(R.string.user_profile_picture_description),
+                    contentScale = ContentScale.Crop
+                )
             }
-        }
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
