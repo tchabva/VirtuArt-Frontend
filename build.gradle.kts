5404b3c18cc84867ea8589773b160982a19a9dcd
Tawanda Chabva <tchabva@gmail.com>
2026-04-13 19:35:54 +0100

    build: update hilt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt.android) version "2.57.2" apply false
+    alias(libs.plugins.hilt.android) version "2.59.2" apply false
     alias(libs.plugins.kotlin.ksp) version "2.3.6" apply false
 }
